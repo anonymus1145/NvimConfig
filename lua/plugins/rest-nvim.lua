d000d0442400be17a41ef2ab4b3cd6dc31681263
Anonymus1145 <Anonymus1145@yahoo.com>
2024-06-13 19:07:57 +0300

    Minor updates

--- lua/plugins/rest-nvim.lua
+++ lua/plugins/rest-nvim.lua
@@ -2,27 +2,18 @@
 return {
   {
     "vhyrro/luarocks.nvim",
-    branch = "go-away-python",
+    priority = 1000,
+    config = true,
     opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }, -- Specify LuaRocks packages to install
-    },
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+    }
   },
   {
     "rest-nvim/rest.nvim",
-    event = "VeryLazy",
-    ft = { "http" },
-    dependencies = {
-      {
-        "luarocks.nvim",
-      },
-    },
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
     config = function()
       require("rest-nvim").setup()
-      require("telescope").load_extension("rest")
     end,
-    keys = {
-      { "<leader>t", "<cmd>Rest run<cr>",                  desc = "Run rest http request under cursor" },
-      { "<leader>ft", "<cmd>Telescope rest select_env<cr>", desc = "Select environment file for rest testing" },
-    },
-  },
+  }
 }
