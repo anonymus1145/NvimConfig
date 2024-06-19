977520b449f8c0aa6ce543cce487d604994ec974
Anonymus1145 <Anonymus1145@yahoo.com>
2024-06-19 22:52:16 +0300

    Update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,28 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "TelescopeResults",
+	callback = function(ctx)
+		vim.api.nvim_buf_call(ctx.buf, function()
+			vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+			vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+		end)
+	end,
+})
+
+local function filenameFirst(_, path)
+	local tail = vim.fs.basename(path)
+	local parent = vim.fs.dirname(path)
+	if parent == "." then return tail end
+	return string.format("%s\t\t%s", tail, parent)
+end
+
+
+
 return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
   },
   {
+
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -13,6 +33,15 @@ return {
             require("telescope.themes").get_dropdown({}),
           },
         },
+
+        pickers = {
+		find_files = {
+			path_display = filenameFirst,
+		}
+	}
+
+
+
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
@@ -22,3 +51,4 @@ return {
     end,
   },
 }
+
