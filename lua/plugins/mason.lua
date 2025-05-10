5b8e60a0abdc2229127624cb1642986fc6581e93
Anonymus1145 <Anonymus1145@yahoo.com>
2025-05-10 14:06:06 +0300

    Minor Updates

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,15 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = { ensure_installed = { "prettier" } },
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
+    opts = {
+      ensure_installed = {
+        "prettier",
+        "eslint-lsp",
+        "js-debug-adapter",
+        "typescript-language-server",
+      },
+    },
   },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
