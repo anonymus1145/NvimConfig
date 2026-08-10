6ab28a22a509f9e750e5e3906f03562aacb203e7
Anonymus1145 <Anonymus1145@yahoo.com>
2026-08-10 06:24:36 +0300

    Sync with remote

--- lua/plugins/rev-nvim.lua
+++ lua/plugins/rev-nvim.lua
@@ -2,6 +2,6 @@ return {
   "anonymus1145/rev.nvim",
   -- Ensure environment variables are set
   cond = function()
-    return os.getenv("GEMINI_API_KEY") ~= nil
+    return (os.getenv("REVNVIM_API_KEY") ~= nil and os.getenv("REVNVIM_MODEL") and os.getenv("REVNVIM_URL"))
   end,
 }
